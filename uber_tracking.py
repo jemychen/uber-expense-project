505214627edd04f87ff9c7db7dd38152087fe19c
Jemy <jemy@Jemys-MacBook-Pro.local>
2022-11-02 23:24:13 -0400

    Added RedShift Hook

--- uber_tracking.py
+++ uber_tracking.py
@@ -4,6 +4,13 @@ import datetime
 # Airflow is an open-source management platform
 from airflow import DAG # DAG is a Directed Acyclic Graph
 
+# Import variable and Connection
+from airflow.models import Variable
+from airflow.models.connection import Connection
+
+# Import PostgresHook
+from airflow.hooks.postgres_hook
+
 dag = DAG (
     'uber_tracking',
     description = 'Tracking expenses via Uber',
@@ -11,3 +18,10 @@ dag = DAG (
     schedule_interval = '@weekly',
     tags = ['UBER']
 )
+
+def fixing_location():
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql_statements.fixing_locations
+    redshift_hook.run(sql_stmt)
+    print(f"Locations fixed successfully.")
+
